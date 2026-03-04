fcded247d8e9e8a32388a3de592226d556322545
student <student@ccm.kfupm.edu.sa>
2026-03-04 08:42:44 +0300

    aaa

--- 5-6-react-styling-fetch-data/src/components/UserList.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserList.jsx
@@ -6,7 +6,7 @@ function UserList({ users, onUserClick }) {
   // TODO 1.3: Empty state — if users.length === 0, RETURN the exact <Alert variant="info">...</Alert>
   if (users.length === 0) {
     return (
-      <Alert>
+      <Alert variant = "info">
         {/* TODO 1.3: Change this Alert to variant="info" and exact message from lab instructions */}
         No users found matching your search criteria.
       </Alert>
